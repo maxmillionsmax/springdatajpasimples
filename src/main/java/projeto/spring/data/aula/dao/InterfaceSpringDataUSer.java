aa9c476c32296b7077cb8a3285650b2d148650e0
maxmillionsmax <maximiliaocaetano@gmail.com>
2022-04-17 01:22:22 -0300

    Bloqueio de objetos com @Lock

--- src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUSer.java
+++ src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUSer.java
@@ -2,6 +2,9 @@ package projeto.spring.data.aula.dao;
 
 import java.util.List;
 
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -14,10 +17,12 @@ import projeto.spring.data.aula.model.UsuarioSpringData;
 @Repository
 public interface InterfaceSpringDataUSer extends CrudRepository<UsuarioSpringData, Long>{
 
+	
 	@Transactional(readOnly = true)
 	@Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
 	public List<UsuarioSpringData> buscaPorNome(String nome);
 	
+	@Lock(LockModeType.READ)
 	@Transactional(readOnly = true)
 	@Query(value = "select p from UsuarioSpringData p where p.nome = :paramnome")
 	public UsuarioSpringData buscaPorNomeParam (@Param("paramnome") String paramnome);
